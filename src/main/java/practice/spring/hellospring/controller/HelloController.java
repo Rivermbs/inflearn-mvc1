56334312644f2f88db4b7fa476c9f9b891959805
mbpark <mbpark@exotech.kr>
2023-11-23 14:23:01 +0900

    Initial commit

---
+++ src/main/java/practice/spring/hellospring/controller/HelloController.java
@@ -0,0 +1,16 @@
+package practice.spring.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "안녕하세요");
+        return "hello";
+    }
+
+}
