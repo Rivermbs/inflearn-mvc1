467780f3beba7ecf6baf720d194e792d121579cb
mbpark <mbpark@exotech.kr>
2023-11-23 18:17:13 +0900

    #5 Lombok 설정

---
+++ src/main/java/practice/spring/hellospring/vo/HelloVO.java
@@ -0,0 +1,12 @@
+package practice.spring.hellospring.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloVO {
+
+    private String name;
+    private int age;
+}
